70432fa33df0d055e9501648f725e6a56995e450
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 11:08:09 +0900

    4

---
+++ MyApp4.java
@@ -0,0 +1,16 @@
+public class MyApp4 {
+  public static void main(String[] args) {
+    /*
+      Javaにおける数値表現
+      author: taro
+    */
+    // System.out.println(10);
+    // System.out.println(-8);
+    // System.out.println(2.5);
+    // System.out.println(1_000_000);
+
+    // eを使った表現
+    System.out.println(1.2e3); //1.2かける10の3乗
+    System.out.println(1.2e-3); //1.2かける10の-3乗
+  }
+}
