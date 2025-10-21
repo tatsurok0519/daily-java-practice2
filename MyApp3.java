047fe7b6438a1bd521cf66e4d436da3c4867868f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 11:04:17 +0900

    3

---
+++ MyApp3.java
@@ -0,0 +1,10 @@
+public class MyApp3 {
+  public static void main(String[] args) {
+    System.out.println(10);
+    System.out.println(-8);
+    System.out.println(2.5);
+    System.out.println(1_000_000);
+    System.out.println(1.2e3);
+    System.out.println(1.2e-3);
+  }
+}
