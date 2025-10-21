c4bd0d9a8972440dc0117aae9210b46e3b22e836
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 11:17:13 +0900

    6

---
+++ MyApp6.java
@@ -0,0 +1,14 @@
+public class MyApp6 {
+  public static void main(String[] args) {
+    // System.out.println(150 * 120);
+    // System.out.println(150 * 130);
+    // System.out.println(150 * 140);
+
+    // int price;
+    // price = 150;
+    int price = 150;
+    System.out.println(price * 120);
+    System.out.println(price * 130);
+    System.out.println(price * 140);
+  }
+}
