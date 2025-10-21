3a5ddda02b82869e5a42b2b326f15ac50530de0d
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 13:15:03 +0900

    14

---
+++ MyApp13.java
@@ -0,0 +1,11 @@
+public class MyApp13 {
+  public static void main(String[] args) {
+    int[] scores = {70, 90, 80};
+    System.out.println(scores[0]);
+    System.out.println(scores[1]);
+    System.out.println(scores[2]);
+    scores[1] = 100;
+    System.out.println(scores[1]);
+    System.out.println(scores.length);
+  }
+}
---
+++ MyApp14.java
@@ -0,0 +1,10 @@
+public class MyApp14 {
+  public static void main(String[] args) {
+    // int[] scores = {70, 90, 80};
+    int[][] scores = {
+      {70, 90, 80},
+      {30, 40, 50},
+    };
+    System.out.println(scores[1][2]);
+  }
+}
