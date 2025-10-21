45dd58336edfaa199e687be8ceb64163acc42396
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 11:21:26 +0900

    7

---
+++ MyApp7.java
@@ -0,0 +1,18 @@
+public class MyApp7 {
+  public static void main(String[] args) {
+    int price = 150;
+    System.out.println(price * 120);
+    System.out.println(price * 130);
+    System.out.println(price * 140);
+
+    // price = 151;
+    // price = price + 1;
+    // price += 1;
+    price++;
+    // price -= 1;
+    // price --;
+    System.out.println(price * 120);
+    System.out.println(price * 130);
+    System.out.println(price * 140);
+  }
+}
