34a8f6ce3d6b565050af041e4b7fb77f3830c544
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-21 13:06:06 +0900

    12

---
+++ MyApp12.java
@@ -0,0 +1,9 @@
+public class MyApp12 {
+  public static void main(String[] args) {
+    // int[] scores = new int[3];
+    // scores[0] = 70;
+    // scores[1] = 90;
+    // scores[2] = 80;
+    int[] scores = {70, 90, 80};
+  }
+}
