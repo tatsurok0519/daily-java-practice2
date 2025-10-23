0f0d0690fc94865cefd2a7b1d6d48a6474e22e3e
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-23 20:03:21 +0900

    44

---
+++ MyApp44.java
@@ -0,0 +1,13 @@
+public class MyApp44 {
+  private static int sum(int a, int b) {
+    return a + b;
+  }
+
+  private static void showSum(int a, int b) {
+    System.out.println(a + b);
+
+  public static void main(String[] args) {
+    System.out.println(sum(3, 7));
+    showSum(3, 7);
+  }
+}
