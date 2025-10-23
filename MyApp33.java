70d7c7067ed51382daf8241d5eb2449a3ded23c5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-23 10:14:38 +0900

    33

---
+++ MyApp33.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MyApp33 {
+  public static void main(String[] args) {
+    int[] scores = {70, -10, 80, 90};
+    for (int score: scores) {
+      if (score < 0) {
+        // break;
+        continue;
+      }
+      System.out.println(score);
+    }
+    System.out.println("Finished");
+  }
+}
