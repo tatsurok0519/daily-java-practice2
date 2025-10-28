979cb3f3d3b1bf5e560055066e6f03476bad29ec
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-29 06:09:36 +0900

    MyApp66

---
+++ MyApp66.java
@@ -0,0 +1,11 @@
+public class MyApp66 {
+  public static void main(String[] args) {
+
+    // int price;
+    // price = 150;
+    int price = 150;
+    System.out.println(price * 120);
+    System.out.println(price * 130);
+    System.out.println(price * 140);
+  }
+}
