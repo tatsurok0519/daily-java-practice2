d83c3c7db07fd93a513a08facb162eaaa911cbf4
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-29 09:40:28 +0900

    MyApp81

---
+++ MyApp81.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MyApp81 {
+  public static void main(String[] args) {
+    System.out.print("Score? ");
+    int num = new Scanner(System.in).nextInt();
+    System.out.println(num);
+  }
+}
