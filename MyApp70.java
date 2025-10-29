0a312822f582cdb8fa03971c1c388659aa5f7f85
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-29 09:07:39 +0900

    MyApp70

---
+++ MyApp70.java
@@ -0,0 +1,7 @@
+public class MyApp70 {
+  public static void main(String[] args) {
+    String fname = "Taro";
+    String lname = "Yamada";
+    System.out.println(String.format("I am %s %s.", fname, lname));
+  }
+}
