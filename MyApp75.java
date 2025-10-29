20befde273c6a1b640956b52193e49a856c3f6c7
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-29 09:26:38 +0900

    MyApp75

---
+++ MyApp75.java
@@ -0,0 +1,11 @@
+public class MyApp75 {
+  public static void main(String[] args) {
+    // int[] firstYearScores = {70, 90, 80};
+    // int[] secondYearScores = {30, 40, 50};
+    int[][] scores = {
+      {70, 90, 80}, // scores[0]
+      {30, 40, 50}, // scores[1]
+    };
+    System.out.println(scores[1][2]);
+  }
+}
