98f0234d47353bcc915c624758268278d62d100f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-29 09:14:43 +0900

    MyApp72

---
+++ MyApp72.java
@@ -0,0 +1,15 @@
+public class MyApp72 {
+  public static void main(String[] args) {
+    // int first = 70;
+    // int second = 90;
+    // int third = 80;
+
+    // int[] scores;
+    // scores = new int[3];
+    // int[] scores = new int[3];
+    // scores[0] = 70;
+    // scores[1] = 90;
+    // scores[2] = 80;
+    int[] scores = {70, 90, 80};
+  }
+}
